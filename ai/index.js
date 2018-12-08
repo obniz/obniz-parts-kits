c46cbe0b42f3ba8308cb45a344da2db84da4a450
木戸 康平 <bouningenn@gmail.com>
2018-12-08 21:19:02 +0900

    add weather list

--- ai/index.js
+++ ai/index.js
@@ -1,5 +1,5 @@
 class ObnizAIHelper {
-  
+
   constructor() {
 
     /* tfjs */
@@ -14,22 +14,24 @@ class ObnizAIHelper {
     this.closestFace = {
       time: null,
       found: false,
-      x:0,
-      y:0,
+      x: 0,
+      y: 0,
       distance: 0
     }
+
+    this.addWeatherList();
   }
 
-  
+
   /* Cam Management */
-  
+
   async startCamWait() {
     this._prepareDOM();
     await this._loadModel();
     await this._startVideo();
     this.cap = new cv.VideoCapture(this.video);
   }
-  
+
   _prepareDOM() {
     var div = document.createElement('div');
     div.innerHTML = `<video id="video_forairobotkit" width="320px" height="240px" autoplay playsinline></video>`;
@@ -38,7 +40,7 @@ class ObnizAIHelper {
     output.appendChild(viodeDOM);
     this.video = document.getElementById("video_forairobotkit");
   }
-  
+
   async _loadCascadeModel() {
     const path = 'haarcascade_frontalface_default.xml';
     const url = 'https://unpkg.com/obniz-parts-kits@0.2.0/ai/opencv3.4/haarcascade_frontalface_default.xml'
@@ -46,10 +48,10 @@ class ObnizAIHelper {
     const buf = await response.arrayBuffer();
     cv.FS_createDataFile('/', path, new Uint8Array(buf), true, false, false);
   }
-  
+
   async _loadModel() {
     /* opencv */
-    
+
     await this._loadCascadeModel();
     this._cv_classifier = new cv.CascadeClassifier();
     const loaded = this._cv_classifier.load('haarcascade_frontalface_default.xml');
@@ -59,16 +61,19 @@ class ObnizAIHelper {
     /* tf */
     this._mobileNet = await mobilenet.load();
   }
-  
+
   _startVideo() {
     const video = this.video;
     return new Promise(async (resolve, reject) => {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: {facingMode: "user"}, audio: false });
+      const stream = await navigator.mediaDevices.getUserMedia({video: {facingMode: "user"}, audio: false});
       video.srcObject = stream;
-      video.onloadedmetadata = (e) => { video.play(); resolve(); };
+      video.onloadedmetadata = (e) => {
+        video.play();
+        resolve();
+      };
     })
   }
-  
+
   /* opencv */
 
   _detectFace() {
@@ -80,14 +85,14 @@ class ObnizAIHelper {
       let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
       let gray = new cv.Mat();
       let faces = new cv.RectVector();
-      
+
       cap.read(src);
-      
+
       // detect faces.
       cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
       let msize = new cv.Size(0, 0);
       this._cv_classifier.detectMultiScale(gray, faces, 1.1, 3, 0, msize, msize);
-  
+
       this.closestFace.found = false;
       this.closestFace.distance = 101;
       for (let i = 0; i < faces.size(); ++i) {
@@ -95,8 +100,8 @@ class ObnizAIHelper {
         const distance = (video.width - face.width) / video.width * 100;
         if (distance < this.closestFace.distance) {
           this.closestFace.found = true;
-          this.closestFace.x = (((face.x + face.width/2)/video.width)*2 - 1)*100;
-          this.closestFace.y = (((face.y + face.height/2)/video.height)*2 - 1)*100;
+          this.closestFace.x = (((face.x + face.width / 2) / video.width) * 2 - 1) * 100;
+          this.closestFace.y = (((face.y + face.height / 2) / video.height) * 2 - 1) * 100;
           this.closestFace.distance = distance;
         }
       }
@@ -106,7 +111,7 @@ class ObnizAIHelper {
       faces.delete();
     }
   }
-  
+
   isFaceInside() {
     this._detectFace();
     return this.closestFace.found;
@@ -129,18 +134,18 @@ class ObnizAIHelper {
       return 101;
     }
   }
-  
+
   positionOfWhiteline() { // reutn -100 to 100. notfound=0
     return 0;
   }
-  
+
   /* tensorflow */
-  
+
   async classify() {
-    if (this.tfclassify.time !== this.video.currentTime ) {
+    if (this.tfclassify.time !== this.video.currentTime) {
       // http://starpentagon.net/analytics/imagenet_ilsvrc2012_dataset/
       const predictions = await this._mobileNet.classify(this.video);
-      for (let i=0; i<predictions.length; i++) {
+      for (let i = 0; i < predictions.length; i++) {
         const name = predictions[i].className;
         const probability = predictions[i].probability;
         this.tfclassify.time = this.video.currentTime;
@@ -150,17 +155,17 @@ class ObnizAIHelper {
     }
     return this.tfclassify.name;
   }
-  
+
   /* speech */
-  
+
   say(mes, rate, pitch) {
     return new Promise((resolve, reject) => {
       const synth = window.speechSynthesis;
       let message = new SpeechSynthesisUtterance(mes);
-      if(typeof rate === "number") {
+      if (typeof rate === "number") {
         message.rate = rate;
       }
-      if(typeof pitch === "number") {
+      if (typeof pitch === "number") {
         message.pitch = pitch;
       }
       message.onend = () => {
@@ -174,14 +179,52 @@ class ObnizAIHelper {
   }
 
   /* API related */
-  
+
   async getWeather(region) {
     /* https://developer.yahoo.com/weather/documentation.html#codes */
-    const res = await fetch("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+region+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
+    const res = await fetch("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + region + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
     const data = await res.json();
-    
+
     const forecast = data.query.results.channel.item.forecast[0].text;
     return forecast;
+  }
+
+  addWeatherList() {
+    this.sunny = [
+      "clear (night)",
+      "sunny",
+    ];
+
+    this.cloudy = [
+      "cloudy",
+      "mostly cloudy (night)",
+      "mostly cloudy (day)",
+      "partly cloudy (night)",
+      "partly cloudy (day)",
+      "partly cloudy",
+    ];
+
+    this.rain = [
+      "mixed rain and snow",
+      "mixed rain and sleet",
+      "freezing drizzle",
+      "drizzle",
+      "freezing rain",
+      "showers",
+      "mixed rain and hail",
+      "scattered showers",
+    ];
+
+    this.snow = [
+      "mixed snow and sleet",
+      "snow flurries",
+      "light snow showers",
+      "blowing snow",
+      "snow",
+      "heavy snow",
+      "scattered snow showers",
+      "snow showers",
+    ];
   }
 
 }
